05a7b5ca7f791d4352edc083a7e5c4d3157f376a
Diallo Amadou Oury <dialloamadououry@MacBook-Pro-de-Diallo.local>
2023-12-18 00:17:51 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+import express from "express";
+import bodyParser  from "body-parser";
+import axios from "axios";
+
+
+const app = express();
+const port = 3000;
+
+let code = "us";
+let headers ={
+    'X-Api-Key': '35abd19df7664a0391cc4e5f7fffa94b',
+};
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const country = ['us', 'ca', 'fr', 'ru', ];
+
+
+app.get("/", async (req, res) =>{
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code}`, {headers});
+        const data = response.data;
+        res.render("index.ejs", {
+            posts: data.articles,
+        })
+    } catch (error) {
+        res.status(400).json({message: "Data not found"});
+    }
+    
+})
+
+app.get("/business", async(req, res) =>{
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code}}&category=business`, {headers});
+        const data = response.data;
+        res.render("index.ejs", {
+            posts: data.articles,
+        })
+    } catch (error) {
+        res.status(400).json({message: "Data not found"});
+    }
+})
+
+app.get("/science", async(req, res) =>{
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code}&category=science`, {headers});
+        const data = response.data;
+        res.render("index.ejs", {
+            posts: data.articles,
+        })
+    } catch (error) {
+        res.status(400).json({message: "Data not found"});
+    }
+})
+
+app.get("/technology", async(req, res) =>{
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code}&category=technology`, {headers});
+        const data = response.data;
+        res.render("index.ejs", {
+            posts: data.articles,
+        })
+    } catch (error) {
+        res.status(400).json({message: "Data not found"});
+    }
+})
+
+app.get("/sports", async(req, res) =>{
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${code}&category=sports`, {headers});
+        const data = response.data;
+        res.render("index.ejs", {
+            posts: data.articles,
+        })
+    } catch (error) {
+        res.status(400).json({message: "Data not found"});
+    }
+})
+
+app.post("/country", (req, res) =>{
+    const countryCode = req.body.country;
+    code = countryCode.toLowerCase();
+    if(req.body.country === ""){
+        res.render("messageError.ejs");
+    }
+    
+    
+    res.redirect("/");
+    
+})
+
+app.listen(port, () =>{
+    console.log(`This server is running on port ${port}`);
+})
